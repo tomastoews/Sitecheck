721507917a8858c064fc906e00aab9c78f59fc65
Tomas Töws <tomas.toews96@gmx.de>
2018-09-26 17:44:54 +0200

    Fixed button, container and navigation for mobile

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,17 @@
-var searchItem=document.querySelector("#search-item"),search=document.querySelector("#search"),messagesItem=document.querySelector("#messages-item"),closeMessages=document.querySelector("#close-messages"),messages=document.querySelector("#messages"),toggleDisplay=function(e){"flex"===e.style.display?e.style.display="none":e.style.display="flex"};searchItem.addEventListener("click",function(){toggleDisplay(search)}),messagesItem.addEventListener("click",function(){toggleDisplay(messages)}),closeMessages.addEventListener("click",function(){toggleDisplay(messages)});
+var searchItem = document.querySelector('#search-item');
+var search = document.querySelector('#search');
+var messagesItem = document.querySelector('#messages-item');
+var closeMessages = document.querySelector('#close-messages');
+var messages = document.querySelector('#messages');
+
+var toggleDisplay = function (element) {
+    if (element.style.display === 'flex') {
+        element.style.display = 'none';
+    } else {
+        element.style.display = 'flex';
+    }
+}
+
+searchItem.addEventListener('click', function() { toggleDisplay(search) });
+messagesItem.addEventListener('click', function () { toggleDisplay(messages) });
+closeMessages.addEventListener('click', function () { toggleDisplay(messages) });
